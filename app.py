55f1d82b6c9497b583e271506b550656335fcd52
ayaannm12 <ayaanm5352@gmail.com>
2025-10-03 09:15:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import random
 random_number = random.randint(1, 100)
-Guess = input("please Enter a random number between 1 and 100: ")
-if int(Guess) == random_number:
-    print("Congratulations! you got the right nmumber")
-    elif int(Guess
+validNumber = False
+solved = False
+while solved == False:
+    while validNumber == False:
+        Guess = input("please Enter a random number between 1 and 100: ")
+        if not input == int:
+                print("Please enter a valid number")
+        else:
+            validNumber = True
+    if int(Guess) == random_number:
+        print("Congratulations! you got the right nmumber")
+        solved = True
+    elif int(Guess) < random_number:
+        print("Your Guess is too Low")
+    else:
+        print("Your guess is too High")
